f4b6717a037037460fa5a905e0a0693036e5ac2e
Enrique Matías Sánchez (Quique) <cronopios@gmail.com>
2014-04-29 20:17:24 +0200

    Provisional home page

---
+++ app/views/hello.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+<!-- {{ $title = "Androcles"; }} -->
+@section('content')
+
+<div class="container">
+    <section class="section-padding">
+        <div class="jumbotron text-center">
+            <img src="images/androcles_logo.jpg" alt="Logo">
+        </div>
+    </section>
+</div>
+
+@stop
