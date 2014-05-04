2e7a4950f4d78b0c7a3455963f2600594d2b7e29
Enrique Matías Sánchez (Quique) <cronopios@gmail.com>
2014-05-04 19:07:05 +0200

    Add script for Facebook's likes

--- app/views/animals/readsingle.blade.php
+++ app/views/animals/readsingle.blade.php
@@ -38,6 +38,14 @@
 @endif
 
 <div class="container col-md-offset-3 col-md-6">
+    <div id="fb-root"></div>
+    <script type="text/javascript">(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/es_ES/all.js#xfbml=1";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
     <div class="fb-like" data-href="{{ URL::current() }}" data-send="true" data-width="450" data-show-faces="true"></div><br />
 
     <p style="margin-top: 20px;">{{ str_replace(["\r\n", "\n", "\r"], '<br />', htmlspecialchars($animal->comments, ENT_QUOTES, 'UTF-8')) }}</p>
