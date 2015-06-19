052e9c93228afa22a13b6ba2be3b8f1ac33965f0
Enrique Matías Sánchez (Quique) <cronopios@gmail.com>
2015-06-19 14:31:13 +0200

    Add Twitter links

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -9,6 +9,11 @@
         </div>
     </section>
 
+    <div class="container" style="text-align: right;">
+        <a href="https://twitter.com/AmigoMioProte" class="twitter-follow-button" data-show-count="false" data-size="large" data-show-screen-name="false">Seguir a @AmigoMioProte</a>
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+    </div>
+
     <div class="container">
         @if (!$news->isEmpty())
             <div class="page-header">
